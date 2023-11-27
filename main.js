a3efff175671ae93a30bd9f2e406d043078083cd
Dylan K <dkooby3@gmail.com>
2023-11-27 13:45:01 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const names = [
+    "Dr. Slice",
+    "Dr. Pressure",
+    "Prof. Possibility",
+    "Prof. Prism",
+    "Dr. Impulse",
+    "Prof. Spark",
+    "Dr. Wire",
+    "Prof. Goose"
+];
+
+const occupations = [
+    "gardener",
+    "programmer",
+    "teacher",
+    "gardner"
+];
+
