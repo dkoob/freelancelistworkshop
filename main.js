6e96b16e892ec71a5819c87c21b446b7d43aa47c
Dylan K <dkooby3@gmail.com>
2023-11-27 14:50:41 -0500

    Finalized Workshop

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// ---------------------------------------------------------------------------
+// data declarations
+
 const names = [
     "Dr. Slice",
     "Dr. Pressure",
@@ -13,6 +16,52 @@ const occupations = [
     "gardener",
     "programmer",
     "teacher",
-    "gardner"
+    "lawncare"
 ];
 
+const freelancerOne = getFreelancer()
+const freelancerTwo = getFreelancer()
+const freelancers = [freelancerOne, freelancerTwo]
+const container = document.querySelector(".container")
+
+// ---------------------------------------------------------------------------
+// function building
+
+function getFreelancer(){
+    const nameNum = Math.floor(Math.random()*names.length)
+    const occupationNum = Math.floor(Math.random()*occupations.length)
+    
+    const freelancer = {
+        name: names[nameNum],
+        occupation: occupations[occupationNum],
+        price: Math.floor(Math.random()*250),
+    }
+    return freelancer
+}
+
+function render(){
+    // freelancers
+    const html = freelancers.map((freelancer) => {
+        return `<p>${freelancer.name} | ${freelancer.occupation} | $${freelancer.price}</p>`
+    })
+    let total = 0
+    container.innerHTML = html.join("")
+    // average price
+    freelancers.forEach((freelancer) =>{
+        total+=freelancer.price;
+    })
+    const averagePrice = document.querySelector(".averagePrice")
+    const averagePriceNum = total/freelancers.length
+    const rounded = averagePriceNum.toFixed(2)
+    averagePrice.innerHTML = (`The average price of a freelancer right now is $${rounded}`)
+}
+
+render()
+const interval = setInterval(() => {
+    const lancer = getFreelancer()
+    freelancers.push(lancer)
+    render()
+    if (freelancers.length === 25){
+        clearInterval(interval)
+    }
+},1500)
